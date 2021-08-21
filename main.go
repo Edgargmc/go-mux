86fca3748b5fed03156e605401d56a6c1a358a4a
Edgar <edgargmc@gmail.com>
2021-08-21 15:00:21 -0300

    add ports

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func main() {
 	app := App{}
 	app.Initialize(
 		os.Getenv("APP_DB_HOST"),
-		5432,
+		getEnvAsInt("APP_DB_PORT", 5432),
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"),
 	)
-	app.Run(":8080")
+	app.Run(os.Getenv("APP_PORT"))
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
+}
+
+func getEnvAsInt(name string, defaultVal int) int {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultVal
 }
